f76aad5b9b7fccb8c34f2b71a87d95fd1960db18
Jose Wynder <wynder.jose@gmail.com>
2026-01-29 17:36:02 -0300

    lesson(14.182): Project configuration

---
+++ src/main/java/io/github/josewynder/rentalcompany/RentalcompanyApplication.java
@@ -0,0 +1,13 @@
+package io.github.josewynder.rentalcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalcompanyApplication.class, args);
+	}
+
+}
